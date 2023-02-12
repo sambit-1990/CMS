d4861bc28a29fec042e9523e11ca3e6bd80ade1b
sambit-1990 <114678631+sambit-1990@users.noreply.github.com>
2023-02-12 11:50:46 +0530

    Commit 2 : TC for Framework Creation
    TC for Framework Creation

--- src/test/java/com/CMS/TestCases/LoginTest.java
+++ src/test/java/com/CMS/TestCases/LoginTest.java
@@ -1,9 +1,20 @@
 package com.CMS.TestCases;
 
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class LoginTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		String driverPath = System.getProperty("user.dir") + "\\Tools\\chromedriver.exe";
+		System.setProperty("webdriver.chrome.driver", driverPath);
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.get("http://www.ginsmartserv.in/CMS/");
 
 	}
 
